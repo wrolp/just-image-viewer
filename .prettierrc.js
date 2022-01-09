c68901ae64e02e26c558cb447cd30edeedcf7f7b
hgvant <hgtk11@163.com>
2022-01-09 21:26:42 +0800

    add prettier configure file

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 120,
+    singleQuote: true,
+    semi: true,
+    tabWidth: 4,
+    useTabs: false,
+    trailingComma: 'none',
+    endOfLine: 'auto',
+    vueIndentScriptAndStyle: true
+}
