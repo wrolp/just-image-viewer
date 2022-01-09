dfa8ebba2d2a308afe5cf12699ba39f93f6742b5
hgvant <hgtk11@163.com>
2022-01-09 21:04:31 +0800

    format files

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,15 @@
-import { expect } from "chai";
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).to.include(msg);
-  });
-});
+
+describe('HelloWorld.vue', () => {
+    it('renders props.msg when passed', () => {
+        const msg = 'new message'
+        const wrapper = shallowMount(HelloWorld, {
+            props: { msg }
+        })
+        expect(wrapper.text()).to.include(msg)
+    })
+})
